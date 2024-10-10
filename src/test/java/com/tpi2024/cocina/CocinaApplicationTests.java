d96c27ae5b1f8fe244946afc3888bf0691255245
Hernán Ismael <ismael29h@gmail.com>
2024-10-10 08:41:32 -0300

    Inicio del proyecto

---
+++ src/test/java/com/tpi2024/cocina/CocinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpi2024.cocina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
