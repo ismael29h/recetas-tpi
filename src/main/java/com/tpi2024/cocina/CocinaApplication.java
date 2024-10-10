a21901273e74e434e0efa1938201b58ee6ee6f1c
Hernán Ismael <ismael29h@gmail.com>
2024-10-10 17:14:56 -0300

    Categorías pre-cargadas para probar

--- src/main/java/com/tpi2024/cocina/CocinaApplication.java
+++ src/main/java/com/tpi2024/cocina/CocinaApplication.java
@@ -1,13 +1,43 @@
 package com.tpi2024.cocina;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.tpi2024.cocina.domain.Categoria;
+import com.tpi2024.cocina.repository.categoria.CategoriaRepository;
 
 @SpringBootApplication
 public class CocinaApplication {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CocinaApplication.class, args);
 	}
 
+	// Categorías precargadas
+	@Bean
+	public CommandLineRunner demoData() {
+		return (args) -> {
+			Categoria categoriaVegana = Categoria.builder()
+					.nombre("Vegana")
+					.build();
+
+			Categoria categoriaVegetariana = Categoria.builder()
+					.nombre("Vegetariana")
+					.build();
+
+			Categoria categoriaCarnes = Categoria.builder()
+					.nombre("Carne")
+					.build();
+
+			categoriaRepository.save(categoriaVegana);
+			categoriaRepository.save(categoriaVegetariana);
+			categoriaRepository.save(categoriaCarnes);
+		};
+	}
 }
