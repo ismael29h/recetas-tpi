2761e62728c55d1b618c18bf3776f87a466689bf
Hernán Ismael <ismael29h@gmail.com>
2024-10-10 17:09:28 -0300

    Lógica de negocio en Receta: crear nueva receta

---
+++ src/main/java/com/tpi2024/cocina/service/receta/RecetaService.java
@@ -0,0 +1,7 @@
+package com.tpi2024.cocina.service.receta;
+
+import com.tpi2024.cocina.dto.receta.RecetaDto;
+
+public interface RecetaService {
+    public RecetaDto createReceta(RecetaDto recetaCreateDto);
+}
---
+++ src/main/java/com/tpi2024/cocina/service/receta/RecetaServiceImpl.java
@@ -0,0 +1,38 @@
+package com.tpi2024.cocina.service.receta;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.tpi2024.cocina.domain.Categoria;
+import com.tpi2024.cocina.domain.Receta;
+import com.tpi2024.cocina.dto.receta.RecetaDto;
+import com.tpi2024.cocina.mapper.receta.RecetaMapper;
+import com.tpi2024.cocina.repository.receta.RecetaRepository;
+import com.tpi2024.cocina.service.categoria.CategoriaService;
+
+@Service
+public class RecetaServiceImpl implements RecetaService {
+    @Autowired
+    private RecetaRepository recetaRepository;
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @Override
+    public RecetaDto createReceta(RecetaDto recetaCreateDto) {
+        // dto -> entidad
+        Receta recetaCreate = RecetaMapper.INSTANCE.recetaDtoToReceta(recetaCreateDto);
+
+        // obtengo categoria por ID y lo asigno (reemplazo) a receta
+        Categoria categoria = categoriaService.getCategoriaById(recetaCreateDto.categoria().id());
+        recetaCreate.setCategoria(categoria);
+
+        // asigno receta a cada paso
+        recetaCreate.getPasos().forEach(paso -> paso.setReceta(recetaCreate));
+
+        // guardar: cascade all
+        Receta recetaCreated = recetaRepository.save(recetaCreate);
+
+        return RecetaMapper.INSTANCE.recetaToRecetaDto(recetaCreated);
+    }
+}
